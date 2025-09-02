0deb0f2c05ddb418c42e2225370f45a58464d724
MaxSlipchuk <166719823+Max3434slipchuk2222@users.noreply.github.com>
2025-09-02 18:32:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+one = 1
+two = 2
+float_one = 1.2
+float_two = 2.4
+text = 'Hello!'
+question = True
+_list = [1,2,3]
+list_tuple = (123,4,432,1)
+mnoz = {8,9,1}
+slovnyk = {'23':'Best','2':'Like'}
+print("Values:")
+print(one)
+print(two)
+print(float_one)
+print(float_two)
+print(text)
+print(question)
+print(_list)
+print(list_tuple)
+print(mnoz)
+print(slovnyk)
+print("Types:")
+print(type(one))
+print(type(two))
+print(type(float_one))
+print(type(float_two))
+print(type(text))
+print(type(question))
+print(type(_list))
+print(type(list_tuple))
+print(type(mnoz))
+print(type(slovnyk))
+
